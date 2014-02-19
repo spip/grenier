3593656ebb6c5774c9a4a1e77f138eba810fa57b
cerf@kraal.fr <>
2014-02-19 04:39:06 +0000

    [Salvatore] [source:_core_/plugins/grenier/lang/ paquet-grenier] Export depuis http://trad.spip.net de la langue fr_tu

---
+++ lang/paquet-grenier_fr_tu.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-grenier?lang_cible=fr_tu
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// G
+	'grenier_description' => 'Fonctions et API dépréciées',
+	'grenier_slogan' => 'Fonctions et API SPIP dépréciées'
+);
+
+?>
