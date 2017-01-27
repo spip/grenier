6625aa1942397698ed145e265d79920f138bb41b
marcimat@rezo.net <>
2017-01-27 17:29:24 +0000

    Mise au placard de "debut_cadre_couleur_foncee" et "fin_cadre_couleur_foncee" non utilisés depuis bien longtemps. On copie aussi ici debut_cadre et fin_cadre sous un autre nom.

--- grenier_fonctions.php
+++ grenier_fonctions.php
@@ -14,6 +14,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
+include_spip('inc/presentation');
 
 /**
  * Filtres d'images
@@ -35,7 +36,7 @@ if (!function_exists('extraire_image_couleur')) {
 }
 
 /**
- * Fonctions d'affichage
+ * Vieilles fonctions d'affichage
  */
 if (!function_exists('afficher_plus')) {
 	/**
@@ -52,4 +53,81 @@ if (!function_exists('afficher_plus')) {
 		include_spip('inc/filtres_ecrire');
 		afficher_plus_info($lien);
 	}
+}
+
+
+if (!function_exists('debut_cadre_couleur_foncee')) {
+	/**
+	 * Ouvre un cadre foncé
+	 * @deprecated utiliser boite_ouvrir()
+	 */
+	function debut_cadre_couleur_foncee($icone = '', $dummy = '', $fonction = '', $titre = '', $id = "", $class = "") {
+		include_spip('inc/presentation');
+		return debut_cadre_grenier('couleur-foncee', $icone, $fonction, $titre, $id, $class);
+	}
+}
+
+if (!function_exists('fin_cadre_couleur_foncee')) {
+	/**
+	 * Ferme un cadre foncé
+	 * @deprecated utiliser boite_fermer()
+	 */
+	function fin_cadre_couleur_foncee() { 
+		return fin_cadre_grenier('couleur-foncee'); 
+	}
+}
+
+
+
+/**
+ * Ouvre un cadre.
+ * Copie de debut_cadre() pour historique
+ */
+function debut_cadre_grenier($style, $icone = "", $fonction = "", $titre = "", $id = "", $class = "", $padding = true) {
+	$style_mapping = array(
+		'r' => 'simple',
+		'e' => 'raccourcis',
+		'couleur' => 'basic highlight',
+		'couleur-foncee' => 'basic highlight',
+		'trait-couleur' => 'important',
+		'alerte' => 'notice',
+		'info' => 'info',
+		'sous_rub' => 'simple sous-rub'
+	);
+	$style_titre_mapping = array('couleur' => 'topper', 'trait-couleur' => 'section');
+	$c = isset($style_mapping[$style]) ? $style_mapping[$style] : 'simple';
+	$class = $c . ($class ? " $class" : "");
+	if (!$padding) {
+		$class .= ($class ? " " : "") . "no-padding";
+	}
+
+	//($id?"id='$id' ":"")
+	if (strlen($icone) > 1) {
+		if ($icone_renommer = charger_fonction('icone_renommer', 'inc', true)) {
+			list($fond, $fonction) = $icone_renommer($icone, $fonction);
+		}
+		$size = 24;
+		if (preg_match("/-([0-9]{1,3})[.](gif|png)$/i", $fond, $match)) {
+			$size = $match[1];
+		}
+		if ($fonction) {
+			// 2 images pour composer l'icone : le fond (article) en background,
+			// la fonction (new) en image
+			$icone = http_img_pack($fonction, "", "class='cadre-icone' width='$size' height='$size'\n" .
+				http_style_background($fond, "no-repeat center center"));
+		} else {
+			$icone = http_img_pack($fond, "", "class='cadre-icone' width='$size' height='$size'");
+		}
+		$titre = $icone . $titre;
+	}
+
+	return boite_ouvrir($titre, $class, isset($style_titre_mapping[$style]) ? $style_titre_mapping[$style] : '', $id);
+}
+
+/**
+ * Ferme un cadre.
+ * Copie de fin_cadre() pour historique
+ */
+function fin_cadre_grenier() { 
+	return boite_fermer(); 
 }
