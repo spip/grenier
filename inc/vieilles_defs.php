677613acdc5cd1797db97d94a6c94569bab26f47
marcimat@rezo.net <>
2018-01-02 21:00:41 +0000

    Ajout de la fonction spip_fetch_array() enlevée de SPIP 3.3-dev. Utiliser sql_fetch() à la place.

--- inc/vieilles_defs.php
+++ inc/vieilles_defs.php
@@ -973,3 +973,38 @@ if (!function_exists('generer_url_retour')) {
 		return rawurlencode(generer_url_ecrire($script, $args, true, true));
 	}
 }
+
+
+if (!function_exists('spip_fetch_array')) {
+	/**
+	 * Retourne une ligne d'un résultat de requête mysql (déprécié)
+	 *
+	 * @see sql_fetch()
+	 * @deprecated Utiliser sql_fetch()
+	 * @param Ressource $r Ressource mysql
+	 * @param int|null $t Type de retour
+	 * @return array|void|bool Tableau de la ligne SQL
+	 **/
+	function spip_fetch_array($r, $t = null) {
+		if (!isset($t)) {
+			if ($r) {
+				return sql_fetch($r);
+			}
+		} else {
+			if ($t == 'SPIP_NUM') {
+				$t = MYSQLI_NUM;
+			}
+			if ($t == 'SPIP_BOTH') {
+				$t = MYSQLI_BOTH;
+			}
+			if ($t == 'SPIP_ASSOC') {
+				$t = MYSQLI_ASSOC;
+			}
+			spip_log("appel deprecie de spip_fetch_array(..., $t)", 'vieilles_defs');
+			if ($r) {
+				return mysqli_fetch_array($r, $t);
+			}
+		}
+	}
+
+}
