36ddbd2059e10c4e1b9a4e5a5fb242569efc7697
marcimat@rezo.net <>
2018-01-02 21:16:40 +0000

    Les filtres "justifier", aligner, aligner_gauche, aligner_droite, centrer et style_align arrivent au grenier. Supprimés de SPIP 3.3-dev. On Utilise du joli code CSS pour faire la même chose de nos jours.

--- grenier_fonctions.php
+++ grenier_fonctions.php
@@ -131,3 +131,97 @@ function debut_cadre_grenier($style, $icone = "", $fonction = "", $titre = "", $
 function fin_cadre_grenier() { 
 	return boite_fermer(); 
 }
+
+
+if (!function_exists('aligner')) {
+	/**
+	 * Alignements en HTML (Old-style, préférer CSS)
+	 *
+	 * Cette fonction ne crée pas de paragraphe
+	 *
+	 * @deprecated Utiliser CSS
+	 * @param string $letexte
+	 * @param string $justif
+	 * @return string
+	 */
+	function aligner($letexte, $justif = '') {
+		$letexte = trim($letexte);
+		if (!strlen($letexte)) {
+			return '';
+		}
+
+		// Paragrapher rapidement
+		$letexte = "<div style='text-align:$justif'>"
+			. $letexte
+			. "</div>";
+
+		return $letexte;
+	}
+}
+
+if (!function_exists('justifier')) {
+	/**
+	 * Justifie en HTML (Old-style, préférer CSS)
+	 *
+	 * @deprecated Utiliser CSS
+	 * @uses aligner()
+	 * @param string $letexte
+	 * @return string
+	 */
+	function justifier($letexte) { 
+		return aligner($letexte, 'justify'); 
+	}
+}
+
+if (!function_exists('aligner_droite')) {
+	/**
+	 * Aligne à droite en HTML (Old-style, préférer CSS)
+	 *
+	 * @deprecated Utiliser CSS
+	 * @uses aligner()
+	 * @param string $letexte
+	 * @return string
+	 */
+	function aligner_droite($letexte) { return aligner($letexte, 'right'); }
+}
+
+if (!function_exists('aligner_gauche')) {
+	/**
+	 * Aligne à gauche en HTML (Old-style, préférer CSS)
+	 *
+	 * @deprecated Utiliser CSS
+	 * @uses aligner()
+	 * @param string $letexte
+	 * @return string
+	 */
+	function aligner_gauche($letexte) { 
+		return aligner($letexte, 'left'); 
+	}
+}
+
+if (!function_exists('centrer')) {
+	/**
+	 * Centre en HTML (Old-style, préférer CSS)
+	 *
+	 * @deprecated Utiliser CSS
+	 * @uses aligner()
+	 * @param string $letexte
+	 * @return string
+	 */
+	function centrer($letexte) { 
+		return aligner($letexte, 'center'); 
+	}
+}
+
+if (!function_exists('style_align')) {
+	/**
+	 * Retourne un texte de style CSS aligné sur la langue en cours
+	 *
+	 * @deprecated
+	 * @param mixed $bof Inutilisé
+	 * @return string Style CSS
+	 **/
+	function style_align($bof) {
+		return "text-align: " . $GLOBALS['spip_lang_left'];
+	}
+}
