d6cd5808f288ceb4d1db650ba11dbd1a195d0e85
Matthieu Marcillaud <marcimat@rezo.net>
2021-05-25 01:03:49 +0200

    Fonction charger_php_extension supprimée de SPIP 4.0

--- inc/grenier_autres.php
+++ inc/grenier_autres.php
@@ -64,3 +64,47 @@ if (!function_exists('spip_fetch_array')) {
 
 }
 
+if (!function_exists('echo_log')) {
+/**
+ * Affiche un code html (echo) et log l'affichage car cet echo est anormal !
+ *
+ * Signale une fonction qui devrait retourner un contenu mais effectue
+ * un echo à la place pour compatibilité ascendante
+ *
+ * @removed from SPIP 4.0
+ * @deprecated
+ *     Utiliser des squelettes pour l'affichage !
+ *
+ * @param string $f
+ *     Nom de la fonction
+ * @param string $ret
+ *     Code HTML à afficher
+ * @return void
+ **/
+function echo_log($f, $ret) {
+	spip_log("Page " . self() . " function $f: echo " . substr($ret, 0, 50) . "...", 'echo');
+	echo(_SIGNALER_ECHOS ? "#Echo par $f#" : "") . $ret;
+}
+}
+
+
+if (!function_exists('charger_php_extension')) {
+/**
+ * Teste la présence d’une extension PHP
+ *
+ * @removed from SPIP 4.0
+ * @deprected Utiliser directement la fonction native `extension_loaded($module)`
+ * @example
+ *     ```
+ *     $ok = charger_php_extension('sqlite');
+ *     ```
+ * @param string $module Nom du module à charger
+ * @return bool true si le module est chargé
+ **/
+function charger_php_extension($module) {
+	if (extension_loaded($module)) {
+		return true;
+	}
+	return false;
+}
+}
