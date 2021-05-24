a3cc555d304d24c9bada37304fad5a30b29a4ec6
Matthieu Marcillaud <marcimat@rezo.net>
2021-05-25 00:43:05 +0200

    Les balises dossier_squelettes et noop supprimées de SPIP 4.0

--- inc/grenier_balises.php
+++ inc/grenier_balises.php
@@ -73,3 +73,49 @@ function balise_FIN_SURLIGNE_dist($p) {
 	return $p;
 }
 }
+
+
+if (!function_exists('balise_DOSSIER_SQUELETTE_dist')) {
+/**
+ * Compile la balise `#DOSSIER_SQUELETTE` retournant le chemin vers le
+ * répertoire de squelettes actuellement utilisé
+ *
+ * @balise
+ * @removed from SPIP 4.0
+ * @deprecated 3.1 Utiliser `#CHEMIN`
+ * @link https://www.spip.net/4627
+ * @see balise_CHEMIN_dist()
+ *
+ * @param Champ $p
+ *     Pile au niveau de la balise.
+ * @return Champ
+ *     Pile completée du code PHP d'exécution de la balise
+ */
+function balise_DOSSIER_SQUELETTE_dist($p) {
+	$code = substr(addslashes(dirname($p->descr['sourcefile'])), strlen(_DIR_RACINE));
+	$p->code = "_DIR_RACINE . '$code'" .
+		$p->interdire_scripts = false;
+
+	return $p;
+}
+}
+
+
+if (!function_exists('balise_NOOP_dist')) {
+/**
+ * Compile la balise `#NOOP`, alias (déprécié) de `#VAL`
+ *
+ * Alias pour regler #948. Ne plus utiliser.
+ *
+ * @balise
+ * @removed from SPIP 4.0
+ * @see balise_VAL_dist()
+ * @deprecated 3.1 Utiliser #VAL
+ *
+ * @param Champ $p
+ *     Pile au niveau de la balise
+ * @return Champ
+ *     Pile complétée par le code à générer
+ **/
+function balise_NOOP_dist($p) { return balise_VAL_dist($p); }
+}
